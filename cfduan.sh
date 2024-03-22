5b112148af33d0f9b77d749057b62e145a4eafd9
hebe061103 <109753786@qq.com>
2024-03-22 14:04:07 +0800

    2024-03-22 14:04:06

--- cfduan.sh
+++ cfduan.sh
@@ -319,7 +319,7 @@ do
 				then
 					let ipcfnum++
 					anycast=$ip
-					echo "$ip:峰值速度$ipcfnum| $max KB/s" |tee -a cfiplist
+					echo "$ip:峰值速度$ipcfnum|$max KB/s" |tee -a cfiplist
 					if [ $ipcfnum -eq 5 ]
 					then
 						status=1
