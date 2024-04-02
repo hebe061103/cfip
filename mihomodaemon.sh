7a359ad8c541f612d4f145d2370f0569b0f346f7
hebe061103 <109753786@qq.com>
2024-04-02 12:53:52 +0800

    2024-04-02 12:53:51

--- mihomodaemon.sh
+++ mihomodaemon.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 while true;
 do
-sleep 30
-if pidof "mihomo" >/dev/null; then
-    echo "mihome is running" >/dev/null 2>&1
-else
-    echo "mihome is stop restart" >/dev/null 2>&1
-    /usr/bin/mihomo -d /usr/local/clash &
+#sleep 30
+name=`pidof mihomo`
+if [[ $name == "" ]]; then
+   /usr/bin/mihomo -d /usr/local/clash &
 fi
 done
+
