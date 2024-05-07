1c31a87680ac416221a123edf3ee7bb9604ca211
hebe061103 <109753786@qq.com>
2024-05-08 07:11:16 +0800

    2024-05-08 07:11:16

--- custonIPlocate.sh
+++ custonIPlocate.sh
@@ -442,6 +442,7 @@ date=$(date "+%Y-%m-%d %H:%M:%S")
 echo --$date-- "------------------------重启CLASH-----------------------------" |tee -a /tmp/cf.log
 rm /usr/local/clash/config.yaml
 cp /tmp/config_cl.yaml  /usr/local/clash/config.yaml
+/usr/bin/killall -9 tmux
 /usr/bin/tmux new-session -d -s cf -n cfw
 /usr/bin/tmux send-keys -t cf:cfw "/usr/bin/mihomo -d /usr/local/clash" C-m  # 设置tmux窗口自动执行的命令 # 使tmux窗口自动进入attached模式 tmux attach -t mysession
 date=$(date "+%Y-%m-%d %H:%M:%S")
