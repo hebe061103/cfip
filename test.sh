a2fe32faa5ccc54782e78792904df586f73c6986
hebe061103 <109753786@qq.com>
2024-12-10 13:31:50 +0800

    2024-12-10 13:31:50

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ colo="Hong Kong,Asia Pacific"
 iplocate=$(echo "$colo" | grep "${address_locate}")
 if [[ "$iplocate" != "" ]]
 then
-    echo "IP所属位置在:$colo"
+    echo "IP所属位置在:${iplocate%,*}"
 else
 	echo "IP所属位置在:$locate 非指定地址选择丢弃."
 fi
